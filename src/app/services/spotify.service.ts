a8796adcf64ce173472334ce07101c8e58f80ca8
William Torres <w.torres@unisimonbolivar.edu.co>
2020-07-27 18:04:46 -0500

    Centraliza las peticiones hacia Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,22 +9,24 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) {
     console.log('SpotifyService start!');
-    }
+  }
 
-  getNewReleases(): any {
+  getQuery( query: string ): any {
+    const url = `https://api.spotify.com/v1/${ query }`;
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQAA3GwfwVN6O0QXHYlNEaw9pM4Q5BPVvf46-kDf1c3_3SGGfYY2Iq1m1iZaGt5hD8QD-TfW2tHnMlMEScw'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-                    .pipe( map( (data: any) => data.albums.items ));
+    return this.http.get(url, { headers });
+
+  }
+
+  getNewReleases(): any {
+    return this.getQuery('browse/new-releases').pipe( map( (data: any) => data.albums.items ));
   }
 
   searchArtist(nameArtist: string): any {
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAA3GwfwVN6O0QXHYlNEaw9pM4Q5BPVvf46-kDf1c3_3SGGfYY2Iq1m1iZaGt5hD8QD-TfW2tHnMlMEScw'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ nameArtist }&type=artist&limit=15`, { headers })
-                    .pipe( map( (data: any) => data.artists.items ));
+    return this.getQuery(`search?q=${ nameArtist }&type=artist&limit=15`)
+               .pipe( map( (data: any) => data.artists.items ));
   }
 
 }
